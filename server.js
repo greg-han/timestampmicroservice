0aafdc88340976debb0e86ffcbe2949dc1aafe8e
greg-han <ghan3@mail.ccsf.edu>
2018-06-03 17:01:37 -0700

    finished timestamp microservice

--- server.js
+++ server.js
@@ -13,6 +13,30 @@ app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+app.get("/api/timestamp/:datestring?",function( req, res, next){
+  var emptytimestring;
+  var timestring = "";
+  if(req.params.datestring != undefined && typeof parseInt(req.params.datestring) === "number" && req.params.datestring.indexOf(".") == -1){
+    timestring = new Date(parseInt(req.params.datestring));
+  }
+  else{
+    timestring = new Date(req.params.datestring);
+  }
+  if(req.params.datestring === undefined || timestring == null){
+   emptytimestring = new Date();
+   res.json({"unix": emptytimestring.getTime(), "utc" : emptytimestring.toUTCString()});
+  }
+  else if(timestring == "Invalid Date"){
+   res.json({"error" : "Invalid Date"});
+  }
+  else{
+   var unixtime = new Date(req.params.datestring).getTime();
+   res.json({"unix" : unixtime,"utc" : timestring.toUTCString()});
+  }
+  next();
+});
+
+
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
